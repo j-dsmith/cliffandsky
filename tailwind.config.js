b3b293cfbf9c6068e471ef2f7da8dbfa535a4cb2
Jamey Smith <smith.jameyd@gmail.com>
2022-04-09 15:49:35 -0500

    build: add tailwind and typescript, setup configs

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      fontFamily: {
+        logo: ["Playfair Display"],
+      },
+    },
+  },
+  plugins: [],
+};
